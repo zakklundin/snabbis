26a04dd0492185150b096d9cac4210136648c4ca
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-09-28 14:39:18 +0200

    Button do be changin background

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function click(){
     document.getElementById('titel').style.color="#0059FF"
+    document.body.style.backgroundColor = "red"
 }
 
 document.getElementById('knapp').onclick = click
+
