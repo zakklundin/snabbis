2f1ecb35d5126f28ce23d44a51ee04ecbae7241b
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-09-28 14:49:35 +0200

    knappen lägger till styling

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function click(){
     document.getElementById('titel').style.color="#0059FF"
     document.body.style.backgroundColor = "red"
+    document.getElementById('titel').innerHTML = "Grattis lol"
+    document.getElementById('titel').style.fontSize = "100px"
+    document.getElementById('knapp').style.height = "100px"
+    document.getElementById('knapp').style.width = "200px"
+    document.getElementById('knapp').style.fontSize = "50px"
+    document.getElementById('knapp').innerHTML = "EPIC"
 }
 
 document.getElementById('knapp').onclick = click
-
